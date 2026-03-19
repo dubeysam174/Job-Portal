26a1ff522ce69274d62ba05148fa578ffe3745e3
dubeysam174 <144706165+dubeysam174@users.noreply.github.com>
2026-03-20 04:09:57 +0530

    fix: corrected base URL to https

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,8 @@
-export const USER_API_END_POINT="http://https://jobx-6vou.onrender.com/api/v1/user";
-export const JOB_API_END_POINT="http://https://jobx-6vou.onrender.com/api/v1/job";
-export const APPLICATION_API_END_POINT="http://https://jobx-6vou.onrender.com/api/v1/application";
-export const COMPANY_API_END_POINT="http://https://jobx-6vou.onrender.com/api/v1/company";
+
+const BASE = "https://jobx-6vou.onrender.com"
+
+export const USER_API_END_POINT = `${BASE}/api/v1/user`;
+export const JOB_API_END_POINT = `${BASE}/api/v1/job`;
+export const APPLICATION_API_END_POINT = `${BASE}/api/v1/application`;
+export const COMPANY_API_END_POINT=`${BASE}:///api/v1/company`;
+// ...all other endpoints
